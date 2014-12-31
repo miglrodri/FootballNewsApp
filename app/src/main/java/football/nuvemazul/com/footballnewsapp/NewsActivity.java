703be50f3aaa7ab5fbc441f0fe2b3882327d1d2f
miglrodri <migl.rodri@gmail.com>
2014-12-31 16:17:07 +0000

    Added NewsActivity Added button to navigate

--- app/src/main/java/football/nuvemazul/com/footballnewsapp/NewsActivity.java
+++ app/src/main/java/football/nuvemazul/com/footballnewsapp/NewsActivity.java
@@ -1,30 +1,24 @@
 package football.nuvemazul.com.footballnewsapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends Activity {
+public class NewsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        new GetContentAsyncTask(this).execute();
+        setContentView(R.layout.activity_news);
     }
 
-    public void jumpToNews(View view) {
-        startActivity(new Intent(this, NewsActivity.class));
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.news, menu);
         return true;
     }
 
@@ -34,6 +28,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
